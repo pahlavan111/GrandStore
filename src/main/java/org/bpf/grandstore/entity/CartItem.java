da2e332eb946fa9fadbe4433cb7173b2af10fdb7
Behrooz <pahlavan111@gmail.com>
2026-06-29 11:35:27 +0330

    Create Cart and CartItem entities

---
+++ src/main/java/org/bpf/grandstore/entity/CartItem.java
@@ -0,0 +1,28 @@
+package org.bpf.grandstore.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "cart_items")
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+
+}
