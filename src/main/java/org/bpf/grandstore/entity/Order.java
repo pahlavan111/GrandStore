3fd5f8fed5720c0513105273f0314dc702f212ed
Behrooz <pahlavan111@gmail.com>
2026-07-19 10:05:54 +0330

    Add order and OrderItem entities

---
+++ src/main/java/org/bpf/grandstore/entity/Order.java
@@ -0,0 +1,37 @@
+package org.bpf.grandstore.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private User customer;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    @Column(name = "created_at", insertable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "total_price")
+    private BigDecimal totalPrice;
+
+}
