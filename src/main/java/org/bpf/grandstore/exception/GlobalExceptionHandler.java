ffeb2544f31d59273b95a72862408c7dd41a92f9
Behrooz <pahlavan111@gmail.com>
2026-07-19 23:34:26 +0330

    add new exception handler for parsing uuid - HttpMessageNotReadableException

--- src/main/java/org/bpf/grandstore/exception/GlobalExceptionHandler.java
+++ src/main/java/org/bpf/grandstore/exception/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package org.bpf.grandstore.exception;
 
+import org.bpf.grandstore.dto.ErrorDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +14,13 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorDto> handleUnReadableMessage(
+            HttpMessageNotReadableException exception
+    ) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorDto("Invalid request body"));
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationError(
@@ -26,25 +35,24 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(CartNotFoundException.class)
-    public ResponseEntity<Map<String, String>> handleCartNotFound(
+    public ResponseEntity<ErrorDto> handleCartNotFound(
             CartNotFoundException exception
     ) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", "Cart not found"));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorDto("Cart not found"));
     }
 
     @ExceptionHandler(ProductNotFoundException.class)
-    public ResponseEntity<Map<String, String>> handleProductNotFound(
+    public ResponseEntity<ErrorDto> handleProductNotFound(
             ProductNotFoundException exception
     ) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", "product not found"));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorDto("product not found"));
     }
 
     @ExceptionHandler(ProductNotFoundInCartException.class)
-    public ResponseEntity<Map<String, String>> handleProductNotFound(
+    public ResponseEntity<ErrorDto> handleProductNotFound(
             ProductNotFoundInCartException exception
     ) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
-                Map.of("error", "Product was not found in the cart"));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorDto("Product was not found in the cart"));
     }
 
     @ExceptionHandler(BadCredentialsException.class)
